1a7a3238c51bea8a2c6677703878d92d2f5630ca
Patryk Bernasiewicz <kontakt@patrykb.pl>
2017-04-21 13:41:14 +0200

    Optimized setting the Pointer max & min rotation values

--- main.js
+++ main.js
@@ -3,38 +3,31 @@
 
 	function Pointer(elementId) {
 		this.element = document.getElementById(elementId);
-		console.log('Element: ', this.element);
+		this.minRotation = parseInt(this.element.getAttribute('data-minRotation')) || -75;
+		this.maxRotation = parseInt(this.element.getAttribute('data-maxRotation')) || 180;
 	}
 	Pointer.prototype.setRotation = function(rotation) {
 		this.element.style.transform = `rotate(${rotation}deg)`;
 	}
-	Pointer.prototype.getMinRotation = function() {
-		return parseInt(this.element.getAttribute('data-minRotation')) || -75;
-	}
-	Pointer.prototype.getMaxRotation = function() {
-		return parseInt(this.element.getAttribute('data-maxRotation')) || 180;
-	}
 
-	var pointers = {
-		main: new Pointer('mainclock'),
-		left: new Pointer('subclock_left'),
-		right: new Pointer('subclock_right')
-	};
+	var pointers = [
+		new Pointer('mainclock'),
+		new Pointer('subclock_left'),
+		new Pointer('subclock_right')
+	];
 
 	var timer = setInterval(function() {
 		var randomRotation;
 		var minRotation;
 		var maxRotation;
 		var rotation;
-		for(var pointer in pointers) {
-			minRotation = pointers[pointer].getMinRotation();
-			maxRotation = pointers[pointer].getMaxRotation();
-			if(minRotation >= 0) {
-				rotation = Math.floor(Math.random() * (maxRotation + minRotation)) - minRotation;
+		for(var pointer of pointers) {
+			if(pointer.minRotation >= 0) {
+				rotation = Math.floor(Math.random() * (pointer.maxRotation + pointer.minRotation)) - pointer.minRotation
 			} else {
-				rotation = Math.floor(Math.random() * (maxRotation + -minRotation)) - -minRotation;
+				rotation = Math.floor(Math.random() * (pointer.maxRotation + -pointer.minRotation)) - -pointer.minRotation
 			}
-			pointers[pointer].setRotation(rotation);
+			pointer.setRotation(rotation);
 		}
 	}, 2000);
 }());
